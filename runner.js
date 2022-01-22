655820390de56b0cd5be471424aa14b23daba99d
Courtney Lewis <Gographicsnow@gmail.com>
2022-01-22 18:27:37 -0500

    Completed assoghment

---
+++ runner.js
@@ -0,0 +1,7 @@
+function sayHiToHeadphonedRoommate(string) {
+    if ("BOB".toLowerCase() === "BOB") {
+        return "I can't hear you!"
+    }
+}
+var word = sayHiToHeadphonedRoommate("Bob")
+    console.log(word)
